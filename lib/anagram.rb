c4d7699b3564dc701b7b6f1f56a4122a777d1162
Jennifer Prince <jennifergraceprince@gmail.com>
2020-06-04 05:55:26 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word_input
+  
+  def initialize(word_input)
+    @word_to_analyze = word_input 
+  end 
+  
+  def match(find_anagram) 
+    puts "Must find #{self.word_input} inside the following string: #{find_anagram}"
+    find_anagram.find_all do |word| 
+      if word.split("").sort == self.word_input.split("").sort 
+        word 
+      end
+    end 
+  end
+  
+end 
